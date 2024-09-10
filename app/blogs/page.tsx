d1973fdee77de3e9154dff74122c624c0c3814e0
gurshaan17 <gurshaansinghd@gmail.com>
2024-09-10 22:51:52 +0530

    button removed

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Link from 'next/link'
-import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Search, UserRoundPen } from 'lucide-react'
 import { useEffect, useState } from 'react'
